f3c0c2d0810f813d47f6810c689c9ee987246f32
firefox35 <f1firefox@yahoo.ie>
2023-07-26 10:18:42 +0000

    Add menu

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-
+from tabulate import tabulate
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -12,10 +12,20 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('cuppa_joe')
 
-sales = SHEET.worksheet('sales')
+def choose():
+        print("****Welcome to Cuppa Joe****\n") 
+        print("Please select from the following menu\n")
+        list = [['Latte', 3.20, 3.50, 3.75],
+                ['Flat White', 2.30, 2.50, 2.80],
+                ['Americano', 2.50, 2.80, 3.10],
+                ['Cappucino', 3.20, 3.50, 3.75],
+                ['Mocha', 3.20, 3.50, 3.75]
+            ]
 
-data = sales.get_all_values()
+        headers = ["Code", "Name","Small","Meduim","Large"]
+        print(tabulate(list, headers, showindex="always", tablefmt="rounded_outline"))
 
-print(data)
-print('Welcome to Cuppa Joe')
- 
+        list = input("Enter here your selection : \n")
+        print("\nYou have selected " + list + " from the menu")
+
+choose()
