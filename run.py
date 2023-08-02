b7d8d911a042fbe65d1135635fe1db9561d9df10
firefox35 <f1firefox@yahoo.ie>
2023-08-02 09:49:20 +0000

    Remove class and amend functions

--- run.py
+++ run.py
@@ -16,60 +16,55 @@ SHEET = GSPREAD_CLIENT.open('cuppa_joe')
 print("****Welcome to Cuppa Joe****\n") 
 waitdress = ['Becky', 'Tisha', 'Cathy','Sarah','Helen']
 print("Hi, My name is " + random.choice(waitdress) + ".\n")
-print("We have a large selection of drinks from our menu.\n")
+print("We have a large selection of drinks available from our menu.\n")
 
-class Item:
 
-    def __init__(self, list, quantity, price):
-        self.list = list
-        self.quantity = quantity
-        self.price = price
+def add_coffee():
+    global list
+    list = [['Latte'],
+            ['Flat White'],
+            ['Americano'],
+            ['Cappucino'],
+            ['Mocha']
+        ]
+    headers = ["Name"]
+    print(tabulate(list, headers, tablefmt="rounded_outline"))
+    list = input("\nWhat coffee would you like off the menu? \n")
 
-class Coffee_Shop:
+add_coffee()
 
-    def __init__(self):
-        self.list_items = []
-        self.order = []
+def quantity():
+        global quantity
+        quantity = int(input(f"\nHow many would {list} you like? \n"))
+   
+quantity() 
 
-    def add_order():
-        
-        list = [['Latte'],
-                ['Flat White'],
-                ['Americano'],
-                ['Cappucino'],
-                ['Mocha']
-            ]
 
-        headers = ["Name"]
-        print(tabulate(list, headers, tablefmt="rounded_outline"))
-        while True:
-            try:
-                order = str(input("\nWhat coffee would you like off the menu? \n"))
-                list.append(order)
-            except TypeError:
-                print('Enter the product name')
-            break
-        quantity = []
-        quantity = str(input("\nHow many " +  order + " would you like? \n"))
-        extra = [["soya milk"], ["cream"], ["chocolate"], ["oat milk"]]
-        headers = ["Extra"]
-        print(tabulate(extra, headers, tablefmt="rounded_outline"))
-        extra = str(input("\nWould you like anything extra with your " + order + "? \n"))
-            
-        size =  [["Small"], ["Meduim"], ["Large"]]
-        headers = ["Size"]
-        print(tabulate(size, headers, tablefmt="rounded_outline"))
-        size = str(input("\nWhat size would you like? \n"))
-        print(f"\nYou have ordered " + quantity +" " + size +" " + order + " with " +  extra  +  " from the menu")
+def add_extra():
+    global extra
+    extra = [["soya milk"], ["cream"], ["chocolate"], ["oat milk"]]
+    headers = ["Extra"]
+    print(tabulate(extra, headers, tablefmt="rounded_outline"))
+    extra = input(f"\nWould you like anything extra with your {list}? \n")
+    
+add_extra()
 
-    add_order()
 
-    def cal_price():
-            price = [
-                    ['Latte', 3.20, 3.50, 3.8],
-                    ['Flat White', 3.20, 3.50, 3.8],
-                    ['Americano', 3.20, 3.50, 3.8],
-                    ['Cappucino', 3.20, 3.50, 3.8],
-                    ['Mocha', 3.20, 3.50, 3.8]
-                ]
+def add_size():
+    global size
+    size = [["Small"], ["Meduim"], ["Large"]]
+    headers = ["Size"]
+    print(tabulate(size, headers, tablefmt="rounded_outline"))
+    size = input("\nWhat size would you like? \n")
+    print (f"\nYou have ordered {quantity} {size} {list} with {extra} from the menu")
 
+add_size()
+
+
+
+def cal_price(self):
+    
+    total = quantity * price
+
+def main():
+    pass
