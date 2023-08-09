255f69473d4a1564a6c7f6a1cd82cb45ab220276
Firefox35 <f1firefox@yahoo.ie>
2023-08-09 00:09:31 +0000

    Amend Code to add to order

--- run.py
+++ run.py
@@ -18,15 +18,16 @@ waitdress = ['Becky', 'Tisha', 'Cathy', 'Sarah', 'Helen']
 print("Hi, My name is " + random.choice(waitdress) + ".\n")
 print("We have a large selection of drinks available from our menu.\n")
 
-coffee_items = {"Latte": {"Small": 2.50, "Meduim": 2.75, "Large": 3.00},
-                "Flat White": {"Small": 2.50, "Meduim": 2.75, "Large": 3.00},
-                "Americano": {"Small": 2.50, "Meduim": 2.75, "Large": 3.00},
-                "Cappucino": {"Small": 2.50, "Meduim": 2.75, "Large": 3.00},
-                "Mocha": {"Small": 2.50, "Meduim": 2.75, "Large": 3.00}}
+coffee_items = {"Latte": {"Small": 2.50, "Medium": 2.75, "Large": 3.00},
+                "Flat White": {"Small": 2.50, "Medium": 2.75, "Large": 3.00},
+                "Americano": {"Small": 2.50, "Medium": 2.75, "Large": 3.00},
+                "Cappucino": {"Small": 2.50, "Medium": 2.75, "Large": 3.00},
+                "Mocha": {"Small": 2.50, "Medium": 2.75, "Large": 3.00}}
 extra_items = {"Soya Milk": .50, "Cream": .75,
                "Chocolate": 1.00, "Oat Milk": .50}
 total = []
-plus = []
+order_items = []
+
  
 
 def add_coffee():
@@ -44,7 +45,7 @@ def add_coffee():
             print("NOT A VALID OPTION!\n")
 
 
-def quantity():
+def get_quantity():
     global quantity
     while True:
         quantity = input(f"\nHow many would {product} you like? \n")
@@ -58,6 +59,7 @@ def quantity():
 
 def add_extra():
     global extra
+    global quantity
     extra = [["Soya milk"], ["Cream"], ["Chocolate"], ["Oat milk"]]
     headers = ["Extra"]
     print(tabulate(extra, headers, tablefmt="rounded_outline"))
@@ -67,15 +69,14 @@ def add_extra():
             if extra in extra_items:
                 break
         except ValueError:
-            extra.lower()
-        else:
             print("Error: Please pick from menu!")
             print()
-    plus.append(extra_items[extra])
+    total.append(extra_items[extra] * quantity)
 
 
 def add_size():
     global size
+    global quantity
     size = [["Small"], ["Meduim"], ["Large"]]
     headers = ["Size"]
     print(tabulate(size, headers, tablefmt="rounded_outline"))
@@ -88,15 +89,7 @@ def add_size():
         except ValueError:
             break
     print(f"\nYou have ordered:\n {quantity} - {size} {product} with {extra}.\n")
-    total.append(coffee_items[product][size])
-
-
-def cal_price():
-    cost = total * int(quantity)
-    price = plus * int(quantity)
-    total_price = price + cost
-    print(f"The total price is:\n €{sum(total_price)}\n")
-
+    total.append(coffee_items[product][size] * quantity)
 
 def order_num():
     print(f"Order number is {random.randint(1,500)}\n")
@@ -105,11 +98,11 @@ def order_num():
 def options():
     menu = [[0, "Add to Order"], [1, "Cancel Order"], [2, "Pay Order"], [3, "Exit"]]
     headers = ["Code", "Options"]
-    print(tabulate(menu, headers, tablefmt="rounded_outline"))
+    print(tabulate(menu, headers,tablefmt="rounded_outline"))
     while True:
         option = int(input("Please enter code: \n"))
         if option in menu[0]:
-            return update_order()
+            return get_order_items()
         elif option in menu[1]:
             break
         elif option in menu[2]:
@@ -117,32 +110,39 @@ def options():
         elif option in menu[3]:
             break
         else:
-            print("Invalid Entry")
-
-def update_order():
-    coffee = add_coffee()
-    coffee.update()
-    amount = int(quantity())
-    amount.update()
-    plus = add_extra()
-    plus.update()
-    size = add_size()
-    size.update()
-    
+            print("Invalid Entry")    
 
 def pay():
     print("Under Construction")
 
-
-def main():
+def get_order_items():
     add_coffee()
-    quantity()
+    get_quantity()
     add_extra()
     add_size()
-    cal_price()
+
+    global quantity
+    global size
+    global extra
+    global product
+
+    order_items.append(f"Quantity: {quantity}\nSize: {size}\nProduct: {product}\nExtra: {extra}\nand\n")
+
+def main():
+    while True:
+        more = input("Do you want to add to the order?\n")
+        if more == "yes":
+            get_order_items()   
+        else:
+            break
+    
+    print(f"The total price is:\n €{sum(total)}\n")
+
+    print("You order:\n")
+    for order in order_items:
+        print(order)
+
     order_num()
-    options()
-    update_order()
-    pay()
+    
 
 main()
